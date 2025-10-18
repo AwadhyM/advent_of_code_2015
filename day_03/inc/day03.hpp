9eccca94dd41943583fde9c43efac1036ab0d40a
Awadhy Mohammed <awadhym@outlook.com>
2025-10-18 10:55:15 +0100

    Set up skeleton for day 03

---
+++ day_03/inc/day03.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+namespace puzzle {
+namespace part_1 {
+int solve_part_1(std::vector<std::string> input);
+}
+
+namespace part_1 {
+int solve_part_2(const std::vector<std::string> input);
+}
+} // namespace puzzle
+
+/*
+ * What the question is asking me to do...
+ *
+ *
+ *
+ * */
---
+++ day_03/src/day03.cpp
@@ -0,0 +1,8 @@
+#include "day01.hpp"
+#include <numeric>
+#include <stdexcept>
+#include <string_view>
+
+int solve_part_1(std::vector<std::string> input) { return 0; }
+
+int solve_part_2(const std::vector<std::string> input) { return 0; }
